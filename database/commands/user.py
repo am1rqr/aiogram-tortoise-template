167445d8aa2e5e4643ab61a2beb09e0f4191c21a
Amir <amir.abzhan@gmail.com>
2024-10-26 22:00:59 +0500

    add

--- database/commands/user.py
+++ database/commands/user.py
@@ -9,3 +9,8 @@ async def select_user_by_id(user_id: int) -> Users:
 async def count_users() -> int:
     users_count = await Users.all().count()
     return users_count
+
+
+async def get_all_users() -> list[Users]:
+    users = await Users.all()
+    return users
