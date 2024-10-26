f070bec09dc34b310a74fc314b0b541f7d126f7d
Amir <amir.abzhan@gmail.com>
2024-10-26 19:10:38 +0500

    add

---
+++ bot/keyboards/builders/back_to.py
@@ -0,0 +1,10 @@
+from aiogram.types import InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+def back_to_builder(callback_data: str) -> InlineKeyboardMarkup:
+    keyboard_builder = InlineKeyboardBuilder()
+    keyboard_builder.button(text='‹ Back', callback_data=callback_data)
+
+    keyboard_builder.adjust(1)
+    return keyboard_builder.as_markup(row_width=1)
