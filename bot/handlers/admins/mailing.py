4e35428fefc721188b2ff9cd28a24aac306be859
Amir <amir.abzhan@gmail.com>
2024-10-26 22:00:54 +0500

    add

--- bot/handlers/admins/mailing.py
+++ bot/handlers/admins/mailing.py
@@ -2,9 +2,9 @@ from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
-from bot.keyboards.builder import back_to_builder
+from bot.keyboards.builders import back_to_builder, approval, approval_builder, button_builder
 from bot.states.admins import Mailing
-from database import commands
+from database.commands.user import get_all_users
 from main import bot
 
 router = Router()
@@ -12,7 +12,7 @@ router = Router()
 
 @router.callback_query(F.data == "mailing")
 async def mailing(call: CallbackQuery, state: FSMContext) -> None:
-    await call.message.edit_text("Send a newsletter to all users:",
+    await call.message.edit_text("Отправьте рассылку всем пользователям:",
                                  reply_markup=back_to_builder("admin_panel"))
     await state.set_state(Mailing.media)
 
@@ -20,47 +20,68 @@ async def mailing(call: CallbackQuery, state: FSMContext) -> None:
 @router.message(Mailing.media)
 async def get_mailing_media(message: Message, state: FSMContext) -> None:
     await state.update_data(text=message.html_text)
-    markup = InlineKeyboardMarkup(inline_keyboard=[
-        [
-            InlineKeyboardButton(text="Approval", callback_data="approval_mailing")
-        ],
-        [
-            InlineKeyboardButton(text="Cancel", callback_data="admin_panel")
-        ]
-    ])
     if message.photo:
         photo = message.photo[-1]
         await state.update_data(photo=photo.file_id)
 
-        await message.answer("Are you sure you want to start sending out a newsletter with this photo and text?",
-                             reply_markup=markup)
+    await message.answer("Если хотите добавить клавиатуру отправьте в формате <i>Текст кнопки:Ссылка</i>",
+                         reply_markup=button_builder("Пропустить", "approval_mailing"))
+    await state.set_state(Mailing.keyboard)
+
+
+@router.message(Mailing.keyboard)
+async def get_mailing_keyboard(message: Message, state: FSMContext) -> None:
+    if ":" not in message.text or message.text.count(":") > 1:
+        await message.answer("Неверный формат клавиатуры, попробуйте еще раз",
+                             reply_markup=back_to_builder("mailing"))
+        return
+
+    keyboard = message.text.split(":")
+    markup = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text=keyboard[0], url=keyboard[1])]
+    ])
+    await state.update_data(markup=markup)
+
+    data = await state.get_data()
+    text: str = data.get("text")
+    photo: str = data.get("photo")
+
+    if photo:
+        await message.answer_photo(photo=photo,
+                                   caption=text,
+                                   reply_markup=markup)
     else:
-        await message.answer(f"Do you really want to start a mailing with the text:\n\n"
-                             f"{message.html_text}",
-                             disable_web_page_preview=True,
+        await message.answer(text,
                              reply_markup=markup)
+
+    await message.answer(f"Вы уверены что хотите начать рассылку?",
+                         reply_markup=approval_builder("approval_mailing", "mailing"))
 
 
 @router.callback_query(F.data == "approval_mailing")
 async def approval_mailing(call: CallbackQuery, state: FSMContext) -> None:
     data = await state.get_data()
-    text = data.get("text")
-    photo = data.get("photo")
+    text: str = data.get("text")
+    photo: str = data.get("photo")
+    markup: InlineKeyboardMarkup = data.get("markup")
 
-    await call.message.edit_text("Mailing in progress...")
+    await call.message.edit_text("<i>Рассылка в процессе...</i>")
 
-    users = await commands.get_all_users()
+    users = await get_all_users()
     for user in users:
         try:
             if photo:
                 await bot.send_photo(chat_id=user.user_id,
                                      photo=photo,
-                                     caption=text)
+                                     caption=text,
+                                     reply_markup=markup)
             else:
                 await bot.send_message(chat_id=user.user_id,
-                                       text=text)
-        except Exception as e:
-            print(e)
+                                       text=text,
+                                       reply_markup=markup)
+        except Exception:
+            pass
 
-    await call.message.answer("Mailing completed successfully")
+    await call.message.answer("<i>Рассылка завершена успешно</i>",
+                              reply_markup=back_to_builder("admin_panel"))
     await state.clear()
